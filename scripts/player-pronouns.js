b974adf2ed6fe1f9f8266651afd27c94caa74f77
Werner <werner.dohse@britehouse.co.za>
2021-05-26 22:31:33 +0200

    feat(module): ensure name shows correct in foundry. change labels. refactor functions

--- scripts/player-pronouns.js
+++ scripts/player-pronouns.js
@@ -2,63 +2,67 @@ let showPlayerList = false;
 let pronounsEnabled = false;
 
 // Player Pronoun Renders
-const PlayerPronounSetup = {
-    getPronoun: function(user) {
-        let pronoun = user.getFlag('player-pronoun', 'pronoun');
-        if (!pronoun) {
-            pronoun = '';
-        }
+function getPronoun(user) {
+    let pronoun = user.getFlag('player-pronouns', 'pronoun');
+    if (!pronoun) {
+        pronoun = '';
+    }
 
-        return pronoun;
-    },
+    return pronoun;
+}
 
-    onConfigRender: function(config, html) {
-        if (pronounsEnabled) {
-            const user = game.users.get(config.user.data._id);
-            const pronoun = PronounSetup.getPronoun(user);
-            const playerColourGroup = html.find('.form-group').eq(1);
+function onConfigRender(config, html) {
+    grabSavedSettings();
 
-            playerColourGroup.after($(`
-                <div class="form-group pronoun">
-                    <label>${game.i18n.localize("PPRN.Enabled.Name")}</label>
-                    <input type="text" name="player-pronoun" value="${pronoun}" data-dtype="String">                    
-                </div>
-            `));
-        }
-	},
+    if (pronounsEnabled) {
+        const user = game.users.get(config.user.data._id);
+        const pronoun = getPronoun(user);
+        const playerColourGroup = html.find('.form-group').eq(1);
 
-    onConfigUpdate: function(config, html) {
-        if (pronounsEnabled) {
-            let pronoun = html.find("input[name = 'player-pronoun']")[0].value;
-
-            const user = game.users.get(config.user.data._id);
-            user.setFlag('player-pronoun', 'pronoun', pronoun);
-        }        
-    },
-
-    renderPlayerList: function(config, html) {
-        if (pronounsEnabled && showPlayerList) {
-            let players = html.find('.player-name');
-        
-            for (let player of players) {
-                let playerCharacterName = player.innerText;
-                const playerName = playerCharacterName.substring(0, playerCharacterName.indexOf('[')).trim();
-
-                const userId = game.users.find((x) => x.data.name === playerName).id;
-                const user = game.users.get(userId);
-                const pronoun = PronounSetup.getPronoun(user);
-
-                if (pronoun) {
-                    const charName = (user.isGM) ? "GM" : user.charname;
-                    player.innerText = `${user.name} (${pronoun}) [${charName}]`;
-                }
-            }
-        }                
+        playerColourGroup.after($(`
+            <div class="form-group pronoun">
+                <label>${game.i18n.localize("PPRN.Label")}</label>
+                <input type="text" name="player-pronouns" value="${pronoun}" data-dtype="String">                    
+            </div>
+        `));
     }
 }
 
+function onConfigUpdate(config, html) {
+    grabSavedSettings();
+
+    if (pronounsEnabled) {
+        let pronoun = html.find("input[name = 'player-pronouns']")[0].value;
+
+        const user = game.users.get(config.user.data._id);
+        user.setFlag('player-pronouns', 'pronoun', pronoun);
+    }        
+}
+
+function renderPlayerList(config, html) {
+    grabSavedSettings();
+
+    if (pronounsEnabled && showPlayerList) {
+        let players = html.find('.player-name');
+    
+        for (let player of players) {
+            let playerCharacterName = player.innerText;
+            const playerName = playerCharacterName.substring(0, playerCharacterName.indexOf('[')).trim();
+
+            const userId = game.users.find((x) => x.data.name === playerName).id;
+            const user = game.users.get(userId);
+            const pronoun = getPronoun(user);
+
+            if (pronoun) {
+                const charName = (user.isGM) ? "GM" : user.charname;
+                player.innerText = `${user.name} (${pronoun}) [${charName}]`;
+            }
+        }
+    }                
+}
+
 function init() {
-    console.log("Player Pronoun initialising...");
+    console.log("Player Pronouns initialising...");
 
     hookupEvents();
 };
@@ -68,7 +72,7 @@ function ready() {
 };
 
 function registerSettings() {
-    game.settings.register("player-pronoun", "enabled", {
+    game.settings.register("player-pronouns", "enabled", {
         name: game.i18n.localize("PPRN.Enabled.Name"),
         hint: game.i18n.localize("PPRN.Enabled.Hint"),
         scope: "world",
@@ -78,7 +82,7 @@ function registerSettings() {
     });
 
     // show in player list
-    game.settings.register("player-pronoun", "showPlayerList", {
+    game.settings.register("player-pronouns", "showPlayerList", {
         name: game.i18n.localize("PPRN.ShowPlayerList.Name"),
         hint: game.i18n.localize("PPRN.ShowPlayerList.Hint"),
         scope: "world",
@@ -89,14 +93,14 @@ function registerSettings() {
 }
 
 function grabSavedSettings() {
-    pronounsEnabled = game.settings.get("player-pronoun", "enabled");
-    showPlayerList = game.settings.get("player-pronoun", "showPlayerList");
+    pronounsEnabled = game.settings.get("player-pronouns", "enabled");
+    showPlayerList = game.settings.get("player-pronouns", "showPlayerList");
 }
 
 function hookupEvents() {
-    Hooks.on("renderPlayerConfig", PlayerPronounSetup.onConfigRender);
-    Hooks.on("closePlayerConfig", PlayerPronounSetup.onConfigUpdate);
-    Hooks.on("renderPlayerList", PlayerPronounSetup.renderPlayerList);
+    Hooks.on("renderPlayerConfig", onConfigRender);
+    Hooks.on("closePlayerConfig", onConfigUpdate);
+    Hooks.on("renderPlayerList", renderPlayerList);
 }
 
 Hooks.once("init", init);
